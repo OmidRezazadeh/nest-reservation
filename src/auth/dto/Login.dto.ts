4ba0d135cdea7a14abe17528d067220458d4040b
orprogrammer <orprogrammer@gmail.com>
2024-12-20 10:47:37 +0330

    feat: enhance LoginDto validation with non-empty checks - Added @IsNotEmpty validation for email and password fields to ensure they are not empty.

--- src/auth/dto/Login.dto.ts
+++ src/auth/dto/Login.dto.ts
@@ -2,13 +2,16 @@ import {
     IsString,
     IsEmail,
     MinLength,
+    IsNotEmpty,
   } from 'class-validator';
   export class LoginDto{
     @IsEmail({},{message:'ایمیل وارد شده صحیح نیست '})
+    @IsNotEmpty({ message: 'ایمیل الزامی است' })
     email:string;
 
   @IsString({ message: 'رمز عبور لازم است و باید یک رشته باشد.' })
   @MinLength(6, { message: 'رمز عبور باید حداقل 6 کاراکتر باشد.' })
+  @IsNotEmpty({ message: 'رمز عبور الزامی است' })
   password: string;
   
 }
