1eb125f83c07108fc055354b6ea8f727dd2dcac8
orprogrammer <orprogrammer@gmail.com>
2025-01-01 19:21:27 +0330

    refactor: remove seeder module and related files
    - Deleted SeederController, SeederService, and SeederModule to simplify the application structure.
    - Removed references to the RolesModule and associated entities from app.module.ts.
    - Cleaned up the application by eliminating unused seeder functionality.

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,6 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
 import { User } from './users/entities/user.entity';
 import { ProductsModule } from './products/products.module';
-import { RolesModule } from './roles/roles.module';
-import { SeederService } from './seeder/seeder.service';
-import { Role } from "src/roles/entities/role.entity";
-import { SeederModule } from './seeder/seeder.module';
 import { AuthModule } from './auth/auth.module';
 import { LoggerMiddleware } from './logger/logger.middleware';
 import { ConfigModule } from '@nestjs/config';
@@ -25,18 +21,16 @@ import { Product } from './products/entities/product.entity';
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
-      entities: [User,Role,Product], // Add all your entities here
+      entities: [User,Product], // Add all your entities here
       synchronize: true, // Don't use in production
       migrations: [__dirname + '/migrations/*{.ts,.js}'],
     }),
     UsersModule,
     ProductsModule,
-    RolesModule,
-    SeederModule,
     AuthModule,
   ],
   controllers: [AppController],
-  providers: [AppService, SeederService],
+  providers: [AppService],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
